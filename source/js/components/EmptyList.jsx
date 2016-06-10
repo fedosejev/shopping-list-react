a6f234f2bab90d2b8f7c420e26235a5f35bb0083
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2016-06-11 00:39:42 +0100

    Update

--- source/js/components/EmptyList.jsx
+++ source/js/components/EmptyList.jsx
@@ -1,13 +1,11 @@
 var React = require('react');
 
-var EmptyList = React.createClass({
-  render: function () {
-    return (
-      <div>
-        There are no items in your list.
-      </div>
-    );
-  }
-});
+var EmptyList = function () {
+  return (
+    <div>
+      There are no items in your list.
+    </div>
+  );
+};
 
 module.exports = EmptyList;
